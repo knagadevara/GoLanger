0eb2879d24445252cd9a388a4d3902fbe091f01e
knagadevara <vnsk.1991@gmail.com>
2020-10-16 05:29:12 +0530

    some test1

--- 999TestinGround/isolate/tester.go
+++ 999TestinGround/isolate/tester.go
@@ -8,6 +8,11 @@ type student struct {
 	yob   uint
 }
 
+type scienceYR struct {
+	student
+	IsEngineer bool
+}
+
 func (st student) fullName() string {
 	return st.fName + " " + st.lName
 }
@@ -16,8 +21,19 @@ func main() {
 
 	// var s2 student
 
-	s1 := student{fName: "Karthik", yob: 1991, lName: "Nagadevara"}
+	s1 := student{fName: "Karthik", lName: "Nagadevara", yob: 1991}
 	s2 := student{"Randy", "Paush", 1971}
-	fmt.Println(s1.fullName())
+
+	eng1 := scienceYR{student: s1, IsEngineer: true}
+	eng2 := scienceYR{
+		student: student{
+			fName: "Jaikanth",
+			lName: "Nagre",
+			yob:   1965,
+		},
+		IsEngineer: false,
+	}
+	fmt.Println(eng1.fullName())
 	fmt.Println(s2.fullName())
+	fmt.Println(eng2.fullName())
 }
