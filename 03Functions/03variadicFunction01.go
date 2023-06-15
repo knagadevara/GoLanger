bd96a5b6bd835e81416307a1e6e767e1d3402a0d
knagadevara <karthik.dzdcode@gmail.com>
2023-06-15 19:57:06 +0530

    wip

--- 03Functions/03variadicFunction01.go
+++ 03Functions/03variadicFunction01.go
@@ -1,15 +1,24 @@
 package main
 
-// MyAverage External Calling demonstrated the capablities of taking unlimited number of arguments of type float64
-// As '...float64' enables the functionality paramerter defination
-// looping over through range function which takes in items and returns index, value
-func MyAverage(numList ...float64) float64 {
-	// fmt.Println(numList)
-	// fmt.Printf("Type of variable: %T \n", numList)
+import "fmt"
+
+// CalculateAverage External available function has the capablities
+// '...float64' as paramerter enables the functionality of taking unlimited
+// number of arguments of type float64 .looping over through range function which takes in items and returns index, value
+func CalculateAverage(numList ...float64) (avg float64) {
+	fmt.Println(numList)
+	fmt.Printf("Type of variable: %T \n", numList)
 	var total float64
 
 	for _, numListValue := range numList {
 		total += numListValue
 	}
 	return total / float64(len(numList))
+}
+
+func main() {
+
+	someList : make([]int , 0)
+	someList = [5,10,31,40,32]
+
 }
