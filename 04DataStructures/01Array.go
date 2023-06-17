0d754fa41ea7fcf17bc6f6a0fcfed5d5918b44d2
knagadevara <vnsk.1991@gmail.com>
2023-06-17 23:32:24 +0530

    Array multi type printing and formating

--- 04DataStructures/01Array.go
+++ 04DataStructures/01Array.go
@@ -4,17 +4,15 @@ import "fmt"
 
 // declaring an array
 
-var MiNami [70]string
+var MiNami [58]int
 
 func main() {
-
-	for i := 65; i <= 134; i++ {
-
-		MiNami[i-65] = string(i)
+	fmt.Printf("Binary\t\tUTF-8-No\tValue\n")
+	fmt.Printf("------\t\t--------\t-----\n")
+	for i := 65; i <= 122; i++ {
+		MiNami[i-65] = i
+		fmt.Printf("%b\t\t%d\t\t%c\n", MiNami[i-65], MiNami[i-65], MiNami[i-65])
 	}
+	fmt.Printf("Element of %T\n", MiNami[0])
 
-	for _, value := range MiNami {
-
-		fmt.Printf("Binary: %b \n", value)
-	}
 }
