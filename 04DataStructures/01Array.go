c75ab3800cb616d7964acdc6323e9585f73a263a
knagadevara <vnsk.1991@gmail.com>
2023-06-18 15:51:54 +0530

    2DArray corrections

--- 04DataStructures/01Array.go
+++ 04DataStructures/01Array.go
@@ -3,15 +3,19 @@ package main
 import "fmt"
 
 // declaring an array
+// type aliases
+// byte --> uin8
+// rune --> int32
 
-var MiNami [58]int
+var MiNami [58]byte
 
 func main() {
+	var txR uint8
 	fmt.Printf("Binary\t\tUTF-8-No\tValue\n")
 	fmt.Printf("------\t\t--------\t-----\n")
-	for i := 65; i <= 122; i++ {
-		MiNami[i-65] = i
-		fmt.Printf("%b\t\t%d\t\t%c\n", MiNami[i-65], MiNami[i-65], MiNami[i-65])
+	for txR = 65; txR <= 122; txR++ {
+		MiNami[txR-65] = txR
+		fmt.Printf("%b\t\t%d\t\t%c\n", MiNami[txR-65], MiNami[txR-65], MiNami[txR-65])
 	}
 	fmt.Printf("Element of %T\n", MiNami[0])
 
--- 04DataStructures/02Slices.go
+++ 04DataStructures/02Slices.go
@@ -17,52 +17,45 @@ var i uint
 //If i is the index of the element to be removed, then the format of this process would look like the following:
 // slice = append(slice[:i], slice[i+1:]...)
 
-//MultiDimensionalSlice demonstrates ways to insert a slice into a slice.
+// MultiDimensionalSlice demonstrates ways to insert a slice into a slice.
 func MultiDimensionalSlice() {
 
 	var student1 = make([]string, 3)
 	var student2 = make([]string, 3)
 	var student3 = make([]string, 3)
 
-	student1[0] = "Karthik"
-	student1[1] = "Nagadevara"
-	student1[2] = "17-JAN-1991"
-
-	student2[0] = "Srinivas"
-	student2[1] = "Palaka"
-	student2[2] = "23-JAN-1985"
-
+	student1[0] = "K"
+	student1[1] = "N"
+	student1[2] = "DOB"
+	student2[0] = "S"
+	student2[1] = "P"
+	student2[2] = "DOB"
 	student3[0] = "Randy"
 	student3[1] = "Paush"
 	student3[2] = "21-DEV-1971"
 
 	// Declaring a multi dimensional slice in short-hand way <- casual approach
 	// Takes slice of string as input but not pointing to an underlying array.
-	StudentYear2018 := [][]string{}
-	//errors out at compile time if accessed directly as below
-
-	// Correct way to do it
+	var StudentYear2018 [][]string
 	StudentYear2018 = append(StudentYear2018, student2)
 
 	// A NIL slice will be created, which does not point to anything yet , till it gets initialized
 	// at this point the underlaying array is not yet created.
 	// No underlying array is created, NIL pointing
-	//Errors out if anything is added is to it, same as above.
-	var StudentYear2019 [][]string
-
 	// Most Prefered and Idiomatic way to make a Slice
 	// reserved spots are already pointed in the memory
 	// an underlying array is already created with the capacity size of 20
+	StudentYear2020 := make([][]string, 0)
 	// Throws an errow if initialized as below
-	// StudentYear2020[0][0] = "Karthik"
-	// StudentYear2020[0][1] = "Nagadevara"
-
-	StudentYear2020 := make([][]string, 10)
+	// StudentYear2020[0][0] = student3[0]
+	// StudentYear2020[0][1] = "Paush"
+	// StudentYear2020[0][1] = "N"
+	// StudentYear2020[0][2] = "DOB"
 	// elements can be directly added
 	StudentYear2020 = append(StudentYear2020, student3)
+	fmt.Printf("%v", StudentYear2020[0][0])
 
 	// Increment a value in the slice
-
 	ExampleSlice := make([]int, 10, 20)
 	ExampleSlice[0] = 1
 	//ExampleSlice[0] = ExampleSlice[0] +1
@@ -73,13 +66,11 @@ func MultiDimensionalSlice() {
 
 func main() {
 	for i = 1; i <= 100; i++ {
-
 		mySlice = append(mySlice, i)
-		fmt.Println("Length: ", len(mySlice), "    Capacity: ", cap(mySlice), "    Value: ", i)
+		fmt.Printf("Length:%d\tCapacity:%d\tValue:%v\n", len(mySlice), cap(mySlice), i)
 	}
 
 	//To delete the element at the position of 56
 	mySlice = append(mySlice[:56], mySlice[57:]...)
-	fmt.Println("Length: ", len(mySlice), "    Capacity: ", cap(mySlice), "    Value: ", mySlice)
-
+	fmt.Printf("Length:%d\tCapacity:%d\nCompleteSliceValues:\t%v\n", len(mySlice), cap(mySlice), mySlice)
 }
